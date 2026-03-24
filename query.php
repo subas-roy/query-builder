f44c75668b68bbaf5d27103bb346e4d963a42ae0
subas-roy <subasroy46@gmail.com>
2026-03-24 10:57:32 +0600

    display posts by specific category

--- query.php
+++ query.php
@@ -3,16 +3,24 @@
 $args = array(
     'post_type'              => array('post'), // use any for any kind of post type, custom post type slug for custom post type
     'post_status'            => array('publish'), // Also support: pending, draft, auto-draft, future, private, inherit, trash, any
-    'posts_per_page'         => '5', // use -1 for all post
+    'posts_per_page'         => -1, // use -1 for all post
     'order'                  => 'DESC', // Also support: ASC
     'orderby'                => 'date', // Also support: none, rand, id, title, slug, modified, parent, menu_order, comment_count
+    'tax_query'                    => [
+        [
+            'taxonomy' => 'category',
+            'field' => 'slug',
+            'terms' => ['cat-1']
+
+        ]
+    ]
 );
 
 // The Query
 $query = new WP_Query($args);
 $i = 1; 
 
-// The Loop
+// The Loop 
 if ($query->have_posts()) {
     while ($query->have_posts()) {
         $query->the_post();
